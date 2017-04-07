33292fd425b4b38dd85a27375ee5ae3b5df9ba30
Ryan Dudley <rdudley4@github.rdudley.xyz>
2017-04-07 12:44:16 -0400

    Initial Project Commit
    1. Upload Initial Project Files
    
    2. Update our Webpack.config.js to utilize V2 syntax when defining loaders. Instead of using the legacy module.loaders we now use module.rules to take advantage of the 'use' property as well as the new query object. This means we no longer have to chain loaders via '!', or specify query parameters within the loader definition. We can chain loader objects within the use property, and place all query parameters in the query property object.
    
    3. Within the webpack-image-loader > file-loader chain, I have updated a few query parameters when sending assets through this pipeline. We now use the query object from webpack v2 to define various options for the file loader & webpack-image-loader, such as:
    
      1. We now include the [path] within the name property for the file-loader, so when the images are optimized and output to the build folder they keep the same file structure as they appear in the src img folder. For example when assets from the img/layout folder are passed through the pipeline, they get output to img/layout, img/platforms etc. Also added the 'context' query property to make sure that the context for [path] is the src folder, not the root of the project.
    
      2. Added query property object to our image-webpack-loader as well. This allows us to fine tune the way we optimize various image types (jpg, png, svg etc). Added the following params:
    
        1. Use progressive rendering.
        2. Optipng -> Optimization Level = 7 [240 trials] (max).
        3. Gifscale -> Output non interlaced gifs.
        4. Mozjpeg -> Set compression quality to 75%.
    
    4. With fine tuned image optimization the image asset file sizes have been drastically reduced from their original versions; without very much quality loss. I would have compressed jpegs even more, but the landing image displayed too much artifacting due to the large display size on the page. That being said the following optimizations were achieved, which should greatly help out on mobile devices, and people with slower internet connections.
    
      1. concert02_no_text.jpg -> Original: 532.7kB | Optimized: 35.9kB = 93.26% Reduction (PogChamp)
      2. lead_singer.jpg -> Original: 195.7kB | Optimized: 27.3kB = 86.05% Reduction (PogChamp)
      3. soul_of_the_frog.jpg -> Original: 637.6kB | Optimized: 163.9kB = 74.29% Reduction (PogChamp)
      4. All SVG images have been optimized as well, with about ~20-25% reduction on average.
    
    5. The project has also been completely rebuilt from scratch to utilize the modular sass methodology with SMACSS, and the BEM naming convention. Initially I was using a fluid grid system as well, but opted to stick with a flexbox based layout in the end. The styling and color scheme has also been completely revamped.

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const version = process.env.npm_package_version;
+
+const webpackConfig = {
+  entry: "./src/js/index.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(jpe?g|gif|svg|png)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            query: {
+              name: '[path]img-[sha512:hash:base64:7].[ext]',
+              context: './src'
+            }
+          },
+          {
+            loader: "image-webpack-loader",
+            query: {
+              progressive: true,
+              optipng: {
+                optimizationLevel: 7
+              },
+              gifscale: {
+                interlaced: false
+              },
+              mozjpeg: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		}),
+    new HtmlWebpackPlugin({
+      filename: 'tours.html',
+      template: 'src/pages/tours.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'survey.html',
+      template: 'src/pages/survey.html'
+    }),
+		new ExtractTextPlugin('application.css')
+	]
+};
+
+module.exports = webpackConfig;
