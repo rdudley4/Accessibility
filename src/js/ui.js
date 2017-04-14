e4a06ffd9e4e456f50d889cb72159e33af844b83
Ryan Dudley <rdudley4@github.rdudley.xyz>
2017-04-14 23:28:05 +0100

    IE/Edge Bug Fixes
    1. object-fit property is not supported natively by IE or Edge,
    because... who knows. This required some additional tweaking to emulate
    the same functionality. An object-fit polyfill has been included into
    the src bundle.js.
    
    2. Since the polyfill is only required on IE/Edge, I have added a simple
    script to detect these browsers, and then implement the polyfill only on
    Edge/IE.
    
    3. Added some styles that are required by the polyfill, and they should
    only be included on IE/Edge as well.

--- src/js/ui.js
+++ src/js/ui.js
@@ -5,19 +5,17 @@
 // Dependencies
 // ------------
 
+import 'objectFitPolyfill/dist/objectFitPolyfill.min';
 require('smoothscroll-polyfill').polyfill();
-const objectFitVideos = require('object-fit-videos/dist/object-fit-videos.min');
-
 
 // Global Variables
 // ----------------
 
 const landing = document.querySelector('.landing') || document.querySelector('.landing--static');
+const landingVideo = document.querySelector('.landing__video');
 const mainContent = document.getElementById('main-content');
 const chevronDown = document.querySelector('.title__chevron-down');
 const toTop = document.getElementById('top');
-
-
 
 // Event Handlers
 // --------------
@@ -43,7 +41,12 @@ if (toTop) {
 }
 
 
-// Object-fit Videos Polyfill
-// --------------------------
+// Object-fit Polyfill
+// -------------------
 
-objectFitVideos();
+if (/Edge\/\d./i.test(navigator.userAgent) || !!navigator.userAgent.match(/Trident\/7\./)){
+   // This is Microsoft Edge or IE 10-11
+   if(landingVideo) {
+     landingVideo.setAttribute('data-object-fit', 'cover');
+   } 
+}
